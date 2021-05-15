10a87c34762dad3952b6cf295ad6c99adb47926f
Mustafamemon <musatafamemon490@gmail.com>
2021-05-15 21:49:12 +0500

    add re train model

--- Automation.py
+++ Automation.py
@@ -3,13 +3,18 @@ import os, re,sys
 import pickle
 import pandas as pd
 
+from io import StringIO
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.preprocessing import LabelEncoder
+
+
 DIR_PATH = os.path.dirname(os.path.abspath("Covid19-Analysis-Forecast"))
 PICKLES_PATH = os.path.join(DIR_PATH, "Pickles")
 # CODE_PATH = os.path.join(DIR_PATH, "Code")
 
 
 
-from fastapi import FastAPI
+from fastapi import BackgroundTasks ,  FastAPI ,  File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
@@ -63,7 +68,43 @@ def models():
     _,_,_models = next(os.walk(PICKLES_PATH))
     return {"models": _models}
 
+def clean_dataset(data):
+    
+    data['corona_result'] = data['corona_result'].replace({"שלילי":"N","חיובי":"Positive","אחר":"Other"})
+    data['gender'] = data['gender'].replace({"זכר":"Male","נקבה":"Female"})
+    
+    
+    data['corona_result'] = LabelEncoder().fit_transform(data['corona_result'])
+    data['age_60_and_above'] = LabelEncoder().fit_transform(data['age_60_and_above'])
+    data['gender'] = LabelEncoder().fit_transform(data['gender'])
 
+    data = data.drop(['test_indication','test_date'],axis=1) 
+
+    data = data.dropna()
+    
+    data = data[data['corona_result']!=1]
+    data['corona_result'].replace({2:1},inplace=True)
+    
+    return data
+@app.post("/train/")
+async def train_model(file: UploadFile = File(...)):
+    data = await file.read()
+
+    data = str(data,'utf-8')
+
+    data = StringIO(data) 
+
+    data = pd.read_csv(data)    
+    
+    data = clean_dataset(data)
+    print(data.head())
+    # Y = data['corona_result']
+    # X = data.drop(["corona_result"],axis=1)
+    # DT = DecisionTreeClassifier()
+    # DT.fit(X,Y)
+    # pickle.dump(DT, open("DTTrained"+str(time.now), 'wb'))
+    
+    return {"filename": file.filename}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8001)
