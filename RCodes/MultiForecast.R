6420ed88abfc89d8081abe65f46381733615c385
noman.anjum <noman.anjum@liberatrade.ai>
2021-05-16 22:27:52 +0500

    More Visualizationis Added

--- RCodes/MultiForecast.R
+++ RCodes/MultiForecast.R
@@ -7,24 +7,27 @@ library(timetk)
 library(ggthemes)
 
 
-
+## Read the aggregated Dataset From Original Data FIle ####
 data = read.csv("/home/noman/B-Drive/University-Docs/Semester8/Assignments/DS Assg/Project/WeeklyCorona.csv")
 
   
   karachidata <- data
   karachidata$test_date = as_date(karachidata$test_date)
   print(karachidata)
+  
+  
+  ## Split Data Set in 80 20 ratio For train and test ##
   splits <- initial_time_split(karachidata, prop = 0.8)
   
   
-  model_fit_arima_no_boost <- arima_reg() %>%
-    set_engine(engine = "auto_arima") %>%
-    fit(corona_result ~ test_date+cough+fever+sore_throat+shortness_of_breath+head_ache, data = training(splits))
+  ### Fitting Training Data To all Models ####
+ ### With equation corona_result ~ test_date+cough+fever+sore_throat+shortness_of_breath+head_ache ####
   
   
   model_fit_arima_no_boost <- arima_reg() %>%
     set_engine(engine = "auto_arima") %>%
     fit(corona_result ~ test_date+cough+fever+sore_throat+shortness_of_breath+head_ache, data = training(splits))
+  
   
   # Model 2: arima_boost ----
   model_fit_arima_boosted <- arima_boost(
@@ -35,9 +38,13 @@ data = read.csv("/home/noman/B-Drive/University-Docs/Semester8/Assignments/DS As
     fit(corona_result ~ test_date+cough+fever+sore_throat+shortness_of_breath+head_ache + as.numeric(test_date) + factor(month(test_date, label = TRUE), ordered = F),
         data = training(splits))
   
+  
+  
   model_fit_ets <- exp_smoothing() %>%
     set_engine(engine = "ets") %>%
     fit(corona_result ~ test_date+cough+fever+sore_throat+shortness_of_breath+head_ache, data = training(splits))
+  
+  
   
   
   model_fit_prophet <- prophet_reg() %>%
@@ -45,10 +52,12 @@ data = read.csv("/home/noman/B-Drive/University-Docs/Semester8/Assignments/DS As
     fit(corona_result ~ test_date+cough+fever+sore_throat+shortness_of_breath+head_ache, data = training(splits))
   
   
+  
   model_fit_lm <- linear_reg() %>%
     set_engine("lm") %>%
     fit(corona_result ~ cough+fever+sore_throat+shortness_of_breath+head_ache+as.numeric(test_date) + factor(month(test_date, label = TRUE), ordered = FALSE),
         data = training(splits))
+  
   
   
   model_spec_mars <- mars(mode = "regression") %>%
@@ -105,7 +114,7 @@ data = read.csv("/home/noman/B-Drive/University-Docs/Semester8/Assignments/DS As
   
   workflow_fit_prophet_boost
   
-  
+  ### Representing All trained Models In One Table ####
   
   model_table <- modeltime_table(
     model_fit_arima_no_boost,
@@ -119,23 +128,31 @@ data = read.csv("/home/noman/B-Drive/University-Docs/Semester8/Assignments/DS As
     workflow_fit_prophet_boost
   )
   
+  
+  ### Claibrating Model Table Over Test Data ###
   model_table
   calibration_table <- model_table %>%
     modeltime_calibrate(testing(splits))
   
   
   
+  ### Forecasting All Models In Table on 20% Test Data  #####
+  
   calibration_table %>%
     modeltime_forecast(actual_data = karachidata,new_data = testing(splits))
   
+  ### Get Error Values Of All Models With Different Error Metrics(ie: MAPE,MASE,MAE) ###
+  
   forecast <- calibration_table %>%
     modeltime_accuracy() 
+  
+  ### Saving Error Table With Sorted MAPE values. Model with least MAPE at top ####
   
   ordered <- forecast[order(forecast$mape),]
   write.csv(ordered,"/home/noman/B-Drive/University-Docs/Semester8/Assignments/DS Assg/Project/WeeklyCoronaAcc.csv")
   
   
-  ## Best Forecast Image Only
+  ## Get Forecast Image By Best Performing Algorithm on Test DAta
   calibration_table1 <- model_table[ordered[1:1,]$.model_id,] %>%
     modeltime_calibrate(testing(splits))
   image<- calibration_table1 %>%
@@ -145,16 +162,23 @@ data = read.csv("/home/noman/B-Drive/University-Docs/Semester8/Assignments/DS As
   plot(image)
   dev.off()
   
+  
+  ## Get Largest Date From Dataset
   max_date =max(as_date(karachidata$test_date))
   max_date
   
+  
+  ### Read Multi Variate Data For Future Forecasting ###
   test_date <- seq(as.Date(max_date)+5, by = 'week', length.out = 13)
   df = read.csv("/home/noman/B-Drive/University-Docs/Semester8/Assignments/DS Assg/Project/WeeklyCoronaTest.csv")
   df = data.frame(test_date,df)
   print("########################")
   print(df)
   print("########################")
-  ## Forecast of best Algo
+  
+  
+  ## Forecast On Future Multi Var Symptom Count Data With Best Performing Model ###
+  ## Get Forecast Image ###
   image <- calibration_table %>%
     filter(.model_id == ordered[1:1,]$.model_id) %>%
     modeltime_refit(karachidata) %>%
@@ -164,7 +188,7 @@ data = read.csv("/home/noman/B-Drive/University-Docs/Semester8/Assignments/DS As
   plot(image)
   dev.off()
   
-  
+  ## Get Forecast Values For Future ###
   Fdata <- calibration_table %>%
     filter(.model_id == ordered[1:1,]$.model_id) %>%
     modeltime_refit(karachidata) %>%
